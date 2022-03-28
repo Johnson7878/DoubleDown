465b856551a0a1b3a3a7178453836da1f14544e4
AvianTheFly <luo.h.michael@gmail.com>
2022-03-28 15:33:32 -0400

    Update generator.java
    Removed paranthesis and division

--- generator.java
+++ generator.java
@@ -5,11 +5,8 @@ public class generator {
     private static Random randomGenerator = new Random();
     private static String operators = "+-*";
     private static int opeatorStringLength = operators.length();
-    private static int answer;
   
-    public static char getRandomOperator() {
-        return operators.charAt(randomGenerator.nextInt(opeatorStringLength));
-    }
+
 
     public static int getRandomNumber() {
         return randomGenerator.nextInt(100);
@@ -22,36 +19,18 @@ public class generator {
         return newEquation;
     }
 
-    public static String createEquation(int numOfOperators) {
+    public static String createEquation(int level) {
         String equation = "";
         char operator;
         int operand;
-        int openParenCounter = 0;
-        for (int i = 0; i < numOfOperators; i++) {
-            operator = getRandomOperator();
-            operand = getRandomNumber();
-            if (operator == '(') {
-                openParenCounter++;
-                equation = appendToEquation(equation, Character.toString(operator), Integer.toString(operand));
-            } else if (operator == ')') {
-                if (openParenCounter == 0) { //Can't start off with a close parenthesis
-                    openParenCounter++;
-                    equation = appendToEquation(equation, "(", Integer.toString(operand));
-                } else {
-                    openParenCounter--;
-                    equation = appendToEquation(equation, Integer.toString(operand), Character.toString(operator));
-                }
-            } else {
-                equation = appendToEquation(equation, Integer.toString(operand), Character.toString(operator));
-            }
+        for (int i = 0; i < level; i++) {
+            operator = operators.charAt(randomGenerator.nextInt(opeatorStringLength));
+            operand = randomGenerator.nextInt(100);
+            equation = appendToEquation(equation, Integer.toString(operand), Character.toString(operator));
         }
-        equation += getRandomNumber();
-        while (openParenCounter > 0) {
-            equation += ")";
-            openParenCounter--;
-        }
+        equation += randomGenerator.nextInt(100);
+
 
         return equation;
     }
-
 }
