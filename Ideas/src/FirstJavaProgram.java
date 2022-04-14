729b0735b237c2ecd82031978752bacab84ecb25
pandeym1 <73703589+pandeym1@users.noreply.github.com>
2022-04-14 09:45:35 -0500

    GUI functionality for log in finished
    Still need to make it look pretty

--- Ideas/src/FirstJavaProgram.java
+++ Ideas/src/FirstJavaProgram.java
@@ -5,10 +5,18 @@ import java.util.Scanner;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptEngine;
 import javax.script.ScriptException;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import java.util.concurrent.TimeUnit;
 import java.math.BigDecimal;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -17,16 +25,78 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.*;
 //first fully functional back end build
-class FirstJavaProgram {
-  
+class FirstJavaProgram implements ActionListener {
+	private static JPanel logInPl;
+	private static JFrame logInFm;
+	private static JLabel logInLab;
+	private static JLabel logInLab2;
+	private static JTextField userText;
+	private static JTextField userNum;
+	private static JButton logInBut;
+	
+	private static String input;
+	private static String inputTemp;
+	public static int inpuNum;
+	public static boolean wait = false;
+	
+	public static int getNum() {
+		return FirstJavaProgram.inpuNum;
+	}
+	
+	public void setNum(int temp) {
+		FirstJavaProgram.inpuNum = temp;
+	}
+	public static String getInput() {
+		return FirstJavaProgram.input;
+	}
+	
+	public void setInput(String temp) {
+		FirstJavaProgram.input = temp;
+	}
+	
   public static void main(String[] args)  throws ScriptException, FileNotFoundException, IOException{
-  
-    while(true){
-        greetUser();
+	  logInPl = new JPanel();
+	  logInFm = new JFrame();
+	  logInFm.setSize(1000,700);
+	  logInFm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	  logInFm.setVisible(true);
+	  logInFm.setResizable(false);
+	  logInFm.add(logInPl);
+	  
+	  logInPl.setLayout(null);
+	  
+	  logInLab = new JLabel("Username:");
+	  logInLab.setBounds(330,350,80,25);
+	  logInPl.add(logInLab);
+	  
+	  logInLab2 = new JLabel("Difficulty Level:");
+	  logInLab2.setBounds(300,380,160,25);
+	  logInPl.add(logInLab2);
+	  
+	  userText = new JTextField();
+	  userText.setBounds(400,350,165,25);
+	  logInPl.add(userText);
+	  
+	  userNum = new JTextField();
+	  userNum.setBounds(400,380,165,25);
+	  logInPl.add(userNum);
+	  
+	  logInBut = new JButton("Submit");
+	  logInBut.setBounds(430,410,105,25);
+	  logInBut.addActionListener(new FirstJavaProgram());
+	  logInPl.add(logInBut);
+	  while (true) {
+	  while (wait == false) {
+		  try {
+		       Thread.sleep(200);
+		    } catch(InterruptedException e) {
+		    }
+	  }
+      greetUser();
         
-        HashMap<String, Student> studentData = new HashMap();
+      HashMap<String, Student> studentData = new HashMap();
 		//Reading in Students.txt objects before program starts
-		try {
+      try {
 			FileInputStream fin = new FileInputStream("Students.txt");
 			ObjectInputStream ois = new ObjectInputStream(fin);
 			while (fin!=null) {
@@ -35,8 +105,7 @@ class FirstJavaProgram {
 			}
 			ois.close();
 		} catch (Exception ex ) {}
-		
-		String name = askUserName();
+		String name = getInput();
 		Student curr;
 		//Fetch or create new user profile
 		if (!studentData.containsKey(name)) {
@@ -51,7 +120,7 @@ class FirstJavaProgram {
 			curr.currScore = 0;
 		}
 		
-        int level = askUserLevel();
+        int level = getNum();
         game game = new game(level);
         game.start_game(curr);
         
@@ -65,9 +134,8 @@ class FirstJavaProgram {
 			Map.Entry mapElement = (Map.Entry)it.next();
 			out.writeObject(mapElement.getValue());
 		}
-    }
-    
-    
+		wait = false;
+	  }
   }
   private static void greetUser(){
     System.out.println("Welcome to our math game!"); 
@@ -79,13 +147,6 @@ class FirstJavaProgram {
     int level = console_level.nextInt();
     return level;
     
-  }
-  
-  static String askUserName() {
-	  Scanner nameIn = new Scanner(System.in);
-	  System.out.println("Please enter your name: ");
-	  String name = nameIn.nextLine();
-	  return name;
   }
     
 	//This function takes in a map and sorts it in descending order
@@ -123,6 +184,43 @@ class FirstJavaProgram {
 				
 			}
 			System.out.println("------------------------------------------");
+		}
+		
+		public static boolean isInteger(String str) {
+		    if (str == null) {
+		        return false;
+		    }
+		    int length = str.length();
+		    if (length == 0) {
+		        return false;
+		    }
+		    int i = 0;
+		    if (str.charAt(0) == '-') {
+		        if (length == 1) {
+		            return false;
+		        }
+		        i = 1;
+		    }
+		    for (; i < length; i++) {
+		        char c = str.charAt(i);
+		        if (c < '0' || c > '9') {
+		            return false;
+		        }
+		    }
+		    return true;
+		}
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if (e.getSource() == logInBut) {
+				input = userText.getText();
+				setInput(input);
+				inputTemp = userNum.getText();
+				if(isInteger(inputTemp) == true) {
+					int foo = Integer.parseInt(inputTemp);
+					setNum(foo);
+					wait = true;
+				}
+			}
 		}
   
 }
